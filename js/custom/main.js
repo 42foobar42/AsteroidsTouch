066a54f34bf4ebf25299e4c1b0a6f63655f067c3
Alexander Boll <alexander.boll@mni.thm.de>
2014-01-18 13:09:58 +0900

    init commit

---
+++ js/custom/main.js
@@ -0,0 +1,9 @@
+function makePlaygroundSize(){
+	var sizeHeader = $("header").height();
+	console.log(sizeHeader);
+	$("div#main").height($("html").height() - sizeHeader);
+}
+
+$( document ).ready(function() {
+	makePlaygroundSize();
+});
