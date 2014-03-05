68edf0f32c85ff4944c83afe7262a54540053229
Alexander Boll <alexander.boll@mni.thm.de>
2014-03-05 10:05:49 +0100

    change to to start the webgl loop

--- js/custom/main.js
+++ js/custom/main.js
@@ -1,10 +1,39 @@
 function makePlaygroundSize(){
 	var sizeHeader = $("header").height();
-	console.log(sizeHeader);
 	$("div#main").height($("html").height() - sizeHeader);
+	console.log("Can: " + $("canvas").height() + " " + $("canvas").width());
+}
+
+function makeObjects(){
+	var temp = new UFO();
+	temp.settranslation(50, 30);
+	//temp.rotate(0.1, 1);
+	Objects.push(temp);
+	temp = new Ship();
+	temp.settranslation(250, 250);
+	temp.setrotation(-0.63, -0.77);
+	Objects.push(temp);	
+	temp = new Laser();
+	temp.settranslation(157, 40);
+	Objects.push(temp);	
+	temp = new BigRock();
+	temp.settranslation(30, 80);
+	Objects.push(temp);	
+	temp = new MediumRock();
+	temp.settranslation(80, 120);
+	Objects.push(temp);
+	temp = new SmallRock();
+	temp.settranslation(180, 70);
+	Objects.push(temp);		
 }
 
 $( document ).ready(function() {
 	makePlaygroundSize();
+	//makeControls();
+	//makeObjects();
+	generateControls();
 	webGLStart();
+	//var obj = new Object();
+	//obj.setitemSize(120);
+	//alert(obj.getitemSize());
 });
